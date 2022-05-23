c2261117e74a37ac29c665c17106da2d72529aa7
dinosaph <andra.oica@yahoo.com>
2022-05-24 00:44:28 +0300

    Project restructuring - Django - UI wip -- Home, Map + ss, gitignore

---
+++ colo_n_coace/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for colo_n_coace project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colo_n_coace.settings')
+
+application = get_asgi_application()
